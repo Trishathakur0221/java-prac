6538b6aa1a0df8d8a833759ec0b64cd909777af1
Trishathakur0221 <trishathakur0221@gmail.com>
2025-05-07 11:21:38 +0530

    day1

---
+++ revarr.java
@@ -0,0 +1,10 @@
+class revarr{
+    public static void main(String[] args) {
+        int arr[]={1,2,4,6,8};
+        for(int i=arr.length-1; i>=0;i--){
+            System.out.print(arr [i]+" ");
+           
+        }
+        
+    }
+}
